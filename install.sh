722431420a7c5c341c6d27104ed3cef878df718a
John Michael Bicada <46289305+Janjan213@users.noreply.github.com>
2019-02-19 20:30:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,29 @@
 #!/bin/zsh
-
-echo apt update
-echo apt upgrade
+#cat>install.sh
+clear
+figlet -f big updating and upgrading packages please wait
+#
+#
+apt update
+apt upgrade
+#
+#
+figlet -f small DONE
+#
+#
+figlet -f small Installing packages please wait...
+#
+#
+apt install python
+apt install python2
+apt install curl
+apt install nano
+apt install micro
+apt install figlet
+apt install cowsay
+#
+#
+figlet -f small DONE
+#
+#
+figlet -f small to be continued
